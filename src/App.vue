ff405fe03c2261ab7b7afc3d6d0a9a8d95557ea0
Marley <mrl.menard@gmail.com>
2024-10-09 20:18:36 -0400

    added AI

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,29 @@
   <div id="app">
     <!-- Only show the sidebar if the current route is not 'login' -->
     <SideBar v-if="!hideSidebar" />
+    <Chatbot v-if="!hideChatbot"/>
     <router-view :fullName="fullName" />
   </div>
 </template>
 
 <script>
 import SideBar from './Components/SideBar.vue'
+import Chatbot from './Components/Chatbot.vue'
 
 export default {
   components: {
-    SideBar
+    SideBar,
+    Chatbot
   },
   computed: {
     // Check if the current route is the login page (or any other route you want to exclude)
     hideSidebar () {
       const routesWithoutSidebar = ['/', '/ForgotPasswd', '/CreateAccount']
       return routesWithoutSidebar.includes(this.$route.path)
+    },
+    hideChatbot () {
+      const routesWithoutChatbot = ['/', '/ForgotPasswd', '/CreateAccount']
+      return routesWithoutChatbot.includes(this.$route.path)
     }
   },
   name: 'App',
---
+++ src/Components/Chatbot.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="chatbot"></div>
+  </template>
+
+<script>
+export default {
+  name: 'Chatbot',
+  mounted () {
+    // Configuration du chatbot
+    window.embeddedChatbotConfig = {
+      chatbotId: 'zmBn9AzY77OmARb78A4Lq',
+      domain: 'www.chatbase.co'
+    }
+
+    // Création d'un script pour le chatbot
+    const script = document.createElement('script')
+    script.src = 'https://www.chatbase.co/embed.min.js'
+    script.setAttribute('chatbotId', 'zmBn9AzY77OmARb78A4Lq')
+    script.setAttribute('domain', 'www.chatbase.co')
+    script.defer = true
+
+    // Ajout du script au document
+    document.body.appendChild(script)
+  },
+  beforeUnmount () {
+    const script = document.querySelector('#chatbase-bubble-button')
+    if (script) {
+      document.body.removeChild(script)
+    }
+  }
+}
+</script>
